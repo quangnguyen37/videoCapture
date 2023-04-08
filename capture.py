dd9a7592ea837d39e463f520e1573a901f1b0164
quangnguyen37 <quanghng37@gmail.com>
2023-04-07 19:26:35 -0700

    Added date time lib to record motion time

--- capture.py
+++ capture.py
@@ -1,11 +1,15 @@
 import cv2, time
+from datetime import datetime
 
 first_frame = None
+status_list = [None,None]
+times=[]
 
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
+    status = 0
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -22,11 +26,17 @@ while True:
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
-
+        status = 1 
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())   
 
     cv2.imshow("Capturing", gray)
     cv2.imshow("Delta",delta_frame)
@@ -36,11 +46,13 @@ while True:
 
 
     key = cv2.waitKey(1)
-    print(gray)
-    print(delta_frame)
 
     if key == ord('q'):
+        if status == 1:
+            times.append(datetime.now())
         break
 
+print(status_list)
+print(times)
 video.release()
-cv2.destroyAllWindowsqq
+cv2.destroyAllWindows
