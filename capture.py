e9dcde945592c135852ca6f9bd428781edf79206
quangnguyen37 <quanghng37@gmail.com>
2023-04-18 19:21:49 -0700

    Implement bokeh plot with motion time

--- capture.py
+++ capture.py
@@ -13,15 +13,13 @@ while True:
     status = 0
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     gray =cv2.GaussianBlur(gray, (21,21),0)
-
     if first_frame is None:
         first_frame = gray
         continue
 
     delta_frame = cv2.absdiff(first_frame,gray) 
-    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 40, 255, cv2.THRESH_BINARY)[1]
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -32,8 +30,8 @@ while True:
         status = 1 
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
-
     status_list.append(status)
+
     if status_list[-1] == 1 and status_list[-2] == 0:
         times.append(datetime.now())
     if status_list[-1] == 0 and status_list[-2] == 1:
@@ -55,6 +53,7 @@ while True:
 
 print(status_list)
 print(times)
+
 for i in range(0, len(times),2):
     df = df.append({"Start":times[i], "End":times[i+1]}, ignore_index = True)
 
